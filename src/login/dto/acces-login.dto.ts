7ad92ca5c1b9db9cf4ea7f8b80f0fcce6d1a6bea
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-06 01:25:23 -0500

    add jwt access

---
+++ src/login/dto/acces-login.dto.ts
@@ -0,0 +1,4 @@
+export class AccessLoginDto {
+    email: string;
+    password: string;
+}
--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constans';
 
 @Module({
   controllers: [LoginController],
   providers: [LoginService, PrismaService],
-  imports: []
+  imports: [
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expired },
+    }),
+
+  ]
 })
 export class LoginModule { }
