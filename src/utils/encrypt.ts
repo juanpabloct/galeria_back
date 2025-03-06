73bde3756a63a62462e68f3f6a6ee9d209290fa0
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-06 00:44:37 -0500

    add hash password

---
+++ src/utils/encrypt.ts
@@ -0,0 +1,12 @@
+
+import * as bcrypt from 'bcrypt';
+
+export const hash = async (password) => {
+    const genSalts = await bcrypt.genSalt();
+
+    return await bcrypt.hash(password, genSalts)
+};
+export const isEqualHash = async (password: string, hash: string) => {
+    const isMatch = await bcrypt.compare(password, hash);
+    return isMatch
+}
