0682608e109e294d8e21dff2c9e4c5bb9c12af05
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-07 21:51:04 -0500

    update album

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,35 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { AlbumPaginationDto } from './dto/pagination-album.dto';
 
-@Controller('/:userId/album')
+@Controller('/user/:userId/album')
 export class AlbumController {
   constructor(private readonly albumService: AlbumService) { }
 
   @Post()
-  async create(@Param("userId") userId: number, @Body() createAlbumDto: CreateAlbumDto) {
+  async create(@Param("userId", ParseIntPipe) userId: number, @Body() createAlbumDto: CreateAlbumDto) {
     return this.albumService.create(createAlbumDto, userId);
   }
 
   @Get()
-  async findAll(@Query() pagination: AlbumPaginationDto) {
-    return this.albumService.findAll(pagination);
+  async findAll(@Param("userId", ParseIntPipe) userId, @Query() pagination: AlbumPaginationDto) {
+    return this.albumService.findAll(userId, pagination);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.albumService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateAlbumDto: UpdateAlbumDto) {
     return this.albumService.update(+id, updateAlbumDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.albumService.remove(+id);
   }
 }
