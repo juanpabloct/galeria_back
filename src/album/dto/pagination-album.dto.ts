c4bd759f811ad0572c9ab929d8ebe4cf9e8f27e2
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-10 08:54:28 -0500

    resolve problem pagination required

--- src/album/dto/pagination-album.dto.ts
+++ src/album/dto/pagination-album.dto.ts
@@ -1,8 +1,10 @@
-import { IsNumber } from "class-validator"
+import { IsNumber, IsOptional } from "class-validator"
 
 export class AlbumPaginationDto {
+    @IsOptional()
     @IsNumber()
     page?: number
+    @IsOptional()
     @IsNumber()
     limit?: number
 }
